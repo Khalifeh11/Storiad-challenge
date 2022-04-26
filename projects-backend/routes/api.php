f2ba0146e4f8c71ce71a9762257de7ec171f167e
Karim <abdelkarim.kh95@gmail.com>
2022-04-26 23:05:37 +0300

    api routes

--- projects-backend/routes/api.php
+++ projects-backend/routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +16,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+
+    Route::post('/add_book', [BookController::class, 'add_book'])->name('add_book');
+    Route::get('/get_all_books', [BookController::class, 'get_all_books'])->name('get_all_books');
+    Route::post('/update_book/{id}', [BookController::class, 'update_book'])->name('update_book');
+    Route::get('/delete_book/{id}', [BookController::class, 'delete_book'])->name('delete_book');
+    Route::post('/add_cover_img', [BookController::class, 'add_cover_img'])->name('add_cover_img');
+    
+
+
+
