ce066d15c73c4c25216e81d4ab1bd138510c9b90
Karim <abdelkarim.kh95@gmail.com>
2022-04-26 18:42:52 +0300

    Books migrations

---
+++ projects-backend/database/migrations/2022_04_26_152616_create_books_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('Title');
+            $table->string('Author');
+            $table->string('Desciption');
+            $table->string('Genre');
+            $table->string('Publisher');
+            $table->string('ISBN');
+            $table->string('Price');
+            $table->string('Pages');
+            $table->date('PublicationDate');
+            $table->string('AmazonLink');
+            $table->string('CoverImg');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+};
